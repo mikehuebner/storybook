91e744fff5f1d88d9619e49a8aa6bf26473de6fe
Valentin Palkovic <valentin@chromatic.com>
2022-12-29 15:05:50 +0100

    Add next/head support

---
+++ code/frameworks/nextjs/src/head-manager/head-manager-provider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { HeadManagerContext } from 'next/dist/shared/lib/head-manager-context';
+import initHeadManager from 'next/dist/client/head-manager';
+
+type HeadManagerValue = {
+  updateHead?: ((state: JSX.Element[]) => void) | undefined;
+  mountedInstances?: Set<unknown>;
+  updateScripts?: ((state: any) => void) | undefined;
+  scripts?: any;
+  getIsSsr?: () => boolean;
+  appDir?: boolean | undefined;
+  nonce?: string | undefined;
+};
+
+const HeadManagerProvider: React.FC = ({ children }) => {
+  const headManager: HeadManagerValue = initHeadManager();
+  headManager.getIsSsr = () => false;
+
+  return <HeadManagerContext.Provider value={headManager}>{children}</HeadManagerContext.Provider>;
+};
+
+export default HeadManagerProvider;
